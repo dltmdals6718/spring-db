e3df29e579c0595f36ef11df1563fcc3fbdede63
LSM <dltmdals6718@naver.com>
2024-05-20 09:54:19 +0900

    데이터 접근 예외 직접 만들기

---
+++ src/main/java/hello/springdb/repository/exception/MyDuplicateKeyException.java
@@ -0,0 +1,20 @@
+package hello.springdb.repository.exception;
+
+public class MyDuplicateKeyException extends MyDbException {
+
+    public MyDuplicateKeyException() {
+        super();
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
